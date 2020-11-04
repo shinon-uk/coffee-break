de9382e5c0def28d9d733bf66c8daccc92f40c8d
hagyyyy1992 <keiichi.hagiwara.1992@gmail.com>
2020-11-04 09:18:51 +0900

    refactor: controllerに移行

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,19 @@
 package controllers
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/astaxie/beego"
+	"log"
+)
+
+var db *sql.DB
+var connectionError error
+
+// DB の接続情報
+const (
+	DriverName = "mysql"
+	DataSourceName = "user:password@tcp(coffee_break_db:3306)/coffee_break"
 )
 
 type MainController struct {
@@ -15,8 +27,38 @@ func (c *MainController) GetContents() {
 
 // メソッド名の先頭が小文字がプライベートメソッド
 func privateFunction() []string {
+	db, err := sql.Open(DriverName, DataSourceName)
+	if err != nil {
+		fmt.Println("open err")
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("ping err")
+		panic(err)
+	}
+	fmt.Println("Get DB Info")
+	rows, err := db.Query("SELECT id, value1, value2 FROM sample")
+	if err != nil {
+		log.Print("error executing database query: ", err)
+		panic(err)
+	}
+	defer rows.Close()
+
+	var id int
+	var value1 string
+	var value2 string
+	for rows.Next() {
+		if err := rows.Scan(&id, &value1, &value2); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, value1, value2)
+	}
+	fmt.Println("done")
+
 	return []string{
-		"value1",
-		"value2",
+		value1,
+		value2,
 	}
 }
--- main.go
+++ main.go
@@ -2,69 +2,14 @@ package main
 
 import (
 	_ "coffee-break/routers"
-	"database/sql"
 	"fmt"
 	"github.com/astaxie/beego"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
-)
-
-var db *sql.DB
-var connectionError error
-
-// DB の接続情報
-const (
-	DriverName = "mysql"
-	DataSourceName = "user:password@tcp(coffee_break_db:3306)/coffee_break"
 )
 
 // 初期化処理
 func init() {
-	db, err := sql.Open(DriverName, DataSourceName)
-	if err != nil {
-		fmt.Println("open err")
-		panic(err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("ping err")
-		panic(err)
-	}
-
-	fmt.Println("Get DB Info")
-	rows, err := db.Query("SELECT id, value1, value2 FROM sample")
-	if err != nil {
-		log.Print("error executing database query: ", err)
-		return
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var value1 string
-		var value2 string
-		if err := rows.Scan(&id, &value1, &value2); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, value1, value2)
-	}
-
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("select ok")
-
-	result, err := db.Exec("UPDATE sample SET value1 = ?, value2 = ? WHERE id = ?", "aaa", "aaa", 1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(result)
-	fmt.Println("update ok")
-
-	defer db.Close()
+	fmt.Println("init")
 }
 
 func main() {
